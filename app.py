8a1d06f75ab4fdde42ff6782067b97c2b81cda1b
Vishwa Vidhatha <vvidhatha@protonmail.com>
2026-02-28 21:57:30 +0530

    Update

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-import static_ffmpeg
-static_ffmpeg.add_paths()
 from flask import Flask, request, jsonify, render_template, redirect, flash, send_file
-from flask_compress import Compress
 import numpy as np
 import pandas as pd
+# import tensorflow as tf
 from tensorflow.keras.models import Model, load_model
 from werkzeug.utils import secure_filename
 import librosa
@@ -11,100 +9,151 @@ import librosa.display
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from PIL import Image
 import os
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
 from pydub import AudioSegment
 from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D,Dropout
 from tensorflow.keras.initializers import glorot_uniform
+import time
+
 
 app = Flask(__name__) 
-Compress(app)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('genre_prediction.html') 
+	return render_template('index.html')
 
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')  
+
+@app.route('/preview',methods=["POST"])
+def preview():
+    if request.method == 'POST':
+        dataset = request.files['datasetfile']
+        df = pd.read_csv(dataset,encoding = 'unicode_escape')
+        return render_template("preview.html",df_view = df)
+
+
+#  multiple custom layers 
 def GenreModel(input_shape = (288,432,4),classes=9):
+    
+    
     X_input = Input(input_shape)
+
     X = Conv2D(8,kernel_size=(3,3),strides=(1,1))(X_input)
     X = BatchNormalization(axis=3)(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((2,2))(X)
+    
     X = Conv2D(16,kernel_size=(3,3),strides = (1,1))(X)
     X = BatchNormalization(axis=3)(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((2,2))(X)
+    
     X = Conv2D(32,kernel_size=(3,3),strides = (1,1))(X)
     X = BatchNormalization(axis=3)(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((2,2))(X)
+
     X = Conv2D(64,kernel_size=(3,3),strides=(1,1))(X)
     X = BatchNormalization(axis=-1)(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((2,2))(X)
+
     X = Conv2D(128,kernel_size=(3,3),strides=(1,1))(X)
     X = BatchNormalization(axis=-1)(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((2,2))(X)
+
     X = Conv2D(256,kernel_size=(3,3),strides=(1,1))(X)
     X = BatchNormalization(axis=-1)(X)
     X = Activation('relu')(X)
     X = MaxPooling2D((2,2))(X)
+
+    
     X = Flatten()(X)
+
     X = Dense(classes, activation='softmax', name='fc' + str(classes), kernel_initializer = glorot_uniform(seed=9))(X)
-    return Model(inputs=X_input,outputs=X,name='GenreModel')
+
+    model = Model(inputs=X_input,outputs=X,name='GenreModel')
+
+    return model
 
 model = GenreModel(input_shape=(288,432,4),classes=9)
 model.load_weights("MLPmusicgen.h5")
 
-class_labels = ['blues', 'classical', 'country', 'disco', 'hiphop', 'metal', 'pop', 'reggae', 'rock']
-
-def predicts(image_data, model):
-    image_array = img_to_array(image_data)
-    image_array = np.reshape(image_array, (1, 288, 432, 4))
-    prediction = model.predict(image_array / 255.0)
-    return np.argmax(prediction), prediction
-
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
-    # --- CRITICAL FIX: Make folder if Render deleted it ---
-    if not os.path.exists('static/tests/'):
-        os.makedirs('static/tests/')
-    
-    audio_file = request.files['audiofile']
-    m_path = os.path.join('static/tests/', secure_filename(audio_file.filename))
-    audio_file.save(m_path)
-    
-    # Handle MP3 or WAV
-    if m_path.lower().endswith(".mp3"):
-        sound = AudioSegment.from_file(m_path)
-        sound.export("static/temp_converted.wav", format="wav")
-        wav = AudioSegment.from_wav("static/temp_converted.wav")
-    else:
-        wav = AudioSegment.from_wav(m_path)
-
-    relevant_segment = wav[40 * 1000: 50 * 1000]
-    relevant_segment.export("static/extracted.wav", format="wav")
-
-    # Generate Spectrogram
-    plt.clf() 
-    y, sr = librosa.load('static/extracted.wav', duration=3)
-    mels = librosa.feature.melspectrogram(y=y, sr=sr)
-    plt.imshow(librosa.power_to_db(mels, ref=np.max))
-    plt.axis('off')
-    plt.savefig('static/melspectrogram.png', bbox_inches='tight', pad_inches=0)    
-
-    image_data = load_img('static/melspectrogram.png', color_mode='rgba', target_size=(288, 432))
-    class_label, _ = predicts(image_data, model)
-    genre = class_labels[class_label]
-    
-    return render_template("genre_prediction.html", prediction=genre, audio_path=m_path)
 
 @app.route('/genre_prediction')
 def genre_prediction():
     return render_template('genre_prediction.html')
+
+class_labels = ['blues', 'classical', 'country', 'disco', 'hiphop', 'metal', 'pop', 'reggae', 'rock']
+
+def convert_mp3_to_wav(music_file):
+    sound = AudioSegment.from_file(music_file)
+    sound.export("static/music_file.wav", format="wav")
+
+def extract_relevant(wav_file, t1, t2):
+    wav = AudioSegment.from_wav(wav_file)
+    relevant_segment = wav[t1 * 1000: t2 * 1000]
+    relevant_segment.export("extracted.wav", format='wav')
+
+def predicts(image_data, model):
+    image_array = img_to_array(image_data)
+    print("Original image_array shape:", image_array.shape)
+    image_array = np.reshape(image_array, (1, 288, 432, 4))
+    prediction = model.predict(image_array / 255.0)
+    class_label = np.argmax(prediction)
+    return class_label, prediction
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    audio_path = request.files['wavfile']
+    print("audio_path", audio_path)
+
+    # securedfile = audio_path.filename
+    # print("securedfile", securedfile)
+
+    input_mp3_file = "input.wav"
+    m_path = os.path.join('static/tests/', secure_filename(audio_path.filename))
+    print("m_path:", m_path)
+    audio_path.save(m_path)
+    time.sleep(6)
+
+    wav = AudioSegment.from_wav(m_path)
+    relevant_segment = wav[40 * 1000: 50 * 1000]
+    relevant_segment.export("static/extracted.wav", format='wav')
+
+    y,sr = librosa.load('static/extracted.wav', duration=3)
+    mels = librosa.feature.melspectrogram(y=y,sr=sr)
+    fig = plt.Figure()
+    canvas = FigureCanvas(fig)
+    p = plt.imshow(librosa.power_to_db(mels,ref=np.max))
+    plt.savefig('static/melspectrogram.png')    
+
+    image_data = load_img('static/melspectrogram.png',color_mode='rgba',target_size=(288,432)) # Image.open('static/melspectrogram.png')
+    class_label, prediction = predicts(image_data, model)
+    genre = class_labels[class_label]
+    print("predict_result:", genre)
+    return render_template("genre_prediction.html", prediction=genre, audio_path=m_path)
+
+
+@app.route('/chart')
+def chart():
+	return render_template('chart.html')
+
+@app.route('/performance')
+def performance():
+	return render_template('performance.html')
 
 if __name__ == "__main__":
     app.run()
